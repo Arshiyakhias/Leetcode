ee6d1981b7c52dd749d1ac4467cbbd270847f9df
Arshiyakhias <116972322+Arshiyakhias@users.noreply.github.com>
2025-07-28 18:39:12 +0530

    Time: 368 ms (67.08%), Space: 0B (100%) - LeetHub

---
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select a.name as employee ##you are renaming the column tp employee because thats what is there in the output 
+from Employee as a
+join
+Employee as b
+on a.managerId=b.id
+where a.salary>b.salary
