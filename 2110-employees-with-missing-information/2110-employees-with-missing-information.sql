2258add90dbb41cbe15608048e967fdff5650d6f
Arshiyakhias <116972322+Arshiyakhias@users.noreply.github.com>
2025-07-29 21:51:39 +0530

    Time: 877 ms (6.48%), Space: 0B (100%) - LeetHub

---
+++ 2110-employees-with-missing-information/2110-employees-with-missing-information.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+
+
+select e.employee_id 
+from Employees as e
+left join
+Salaries as s
+on e.employee_id = s.employee_id
+where s.employee_id is null
+union
+select s.employee_id 
+from Employees as e
+right join
+Salaries as s
+on s.employee_id =e.employee_id
+where e.employee_id is null
+order by employee_id asc
