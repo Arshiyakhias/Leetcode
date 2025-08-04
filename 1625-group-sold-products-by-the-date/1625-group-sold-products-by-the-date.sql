e4611ed45fb4b3841f21c2bedf0d91ad5c553e4c
Arshiyakhias <116972322+Arshiyakhias@users.noreply.github.com>
2025-08-05 00:41:18 +0530

    Time: 441 ms (60.18%), Space: 0B (100%) - LeetHub

--- 1625-group-sold-products-by-the-date/1625-group-sold-products-by-the-date.sql
+++ 1625-group-sold-products-by-the-date/1625-group-sold-products-by-the-date.sql
@@ -3,3 +3,5 @@
 select sell_date,count(distinct product) as num_sold,group_concat(distinct product order by product) as products
 from Activities 
 group by sell_date
+
+#group_concat(distinct product order by product) groups products and orders it in alphabetical order
