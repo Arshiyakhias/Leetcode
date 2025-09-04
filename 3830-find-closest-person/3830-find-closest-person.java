d4b74c8f8626eff373af14bbebcaf0c8cb1ba3e4
Arshiyakhias <116972322+Arshiyakhias@users.noreply.github.com>
2025-09-04 20:36:30 +0530

    Time: 1 ms (34.71%), Space: 41.5 MB (5.62%) - LeetHub

--- 3830-find-closest-person/3830-find-closest-person.java
+++ 3830-find-closest-person/3830-find-closest-person.java
@@ -1,37 +1,17 @@
 class Solution {
     public int findClosest(int x, int y, int z) {
         HashMap<Integer,Integer>map=new HashMap<>();
-        map.put(x,1);
-        map.put(y,2);
-       
-
-
-
-        ArrayList<Integer>lis=new ArrayList<Integer>();
-        lis.add(x);
-        lis.add(y);
-        Boolean[]flag;
-        flag=new Boolean[1];
-        flag[0]=false;
-        Collections.sort(lis,(a,b)->{
-        int val=Integer.compare(Math.abs(a-z),Math.abs(b-z));
-        {
-            if(val==0){
-                flag[0]=true;
-                return 0;
-            }
+        int a=0;
+        int b=0;
+        a=Math.abs(x-z);
+        b=Math.abs(y-z);
+        if(a<b){
+            return 1;
         }
-        return val;
-       
-        });
-
-        // when you have a conditional statement in the lambda expression then wrap it with {}
-        //flag like above cannot be called from outside hence using array to define it
-        if(flag[0]==true){
-            return 0;
+        else if(b<a){
+            return 2;
         }
-        return map.get(lis.get(0));
-        
-        
+        return 0;
+
     }
 }
