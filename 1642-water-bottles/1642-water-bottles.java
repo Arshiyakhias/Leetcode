a23337752946f9715b54e32f1a349ed9abeb8808
Arshiyakhias <116972322+Arshiyakhias@users.noreply.github.com>
2025-10-01 22:46:39 +0530

    Time: 0 ms (100%), Space: 40.6 MB (43.3%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int sum=numBottles;
+        
+        while(numBottles/numExchange!=0){
+            sum=sum+(numBottles/numExchange);
+            numBottles=(numBottles/numExchange)+(numBottles%numExchange);
+           
+        }
+        return sum;
+
+    }
+}
