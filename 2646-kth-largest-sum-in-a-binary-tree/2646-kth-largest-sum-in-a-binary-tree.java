4392382d3ab2a8700b991d0782b1ebc55f171b9b
Arshiyakhias <116972322+Arshiyakhias@users.noreply.github.com>
2025-10-06 21:21:16 +0530

    Time: 69 ms (23.18%), Space: 81.3 MB (5.27%) - LeetHub

---
+++ 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public long kthLargestLevelSum(TreeNode root, int k) {
+        List<List<Integer>>lis=new ArrayList<>();
+        Queue<TreeNode>q=new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int vals=q.size();
+            List<Integer>addon=new ArrayList<>();
+            while(vals!=0){
+                TreeNode check=q.poll();
+                if(check.left!=null){
+                    q.add(check.left);
+                }
+                if(check.right!=null){
+                    q.add(check.right);
+                }
+                addon.add(check.val);
+                vals--;
+            }
+            lis.add(addon);
+        }
+        List<Long>fin=new ArrayList<>();
+        
+        for(List<Integer>sing:lis){
+            long sum=0;
+            for(int i=0;i<sing.size();i++){
+                sum=sum+sing.get(i);
+
+            }
+           fin.add(sum);
+        }
+        Collections.sort(fin);
+        List<Long>last=new ArrayList<>();
+        for(int i=fin.size()-1;i>=0;i--){
+            
+                last.add(fin.get(i));
+
+            
+           
+        }
+        if(last.size()<k){
+            return -1;
+        }
+
+        return last.get(k-1);
+
+    }
+}
