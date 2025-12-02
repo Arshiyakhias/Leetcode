bc05c73a20977226f372dcbdbee894a031327b5e
Arshiyakhias <116972322+Arshiyakhias@users.noreply.github.com>
2025-12-02 22:39:49 +0530

    Time: 36 ms (22.31%), Space: 48.2 MB (6.01%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,82 @@
+class Solution {
+    public boolean checkInclusion(String p, String s) {
+        int k=p.length();
+       
+       if(s.length()<p.length()){
+        return false;
+       }
+      String check=s.substring(0,k);
+    
+       StringBuilder str=new StringBuilder(check);
+       HashMap<Character,Integer>map=new HashMap<>();
+       for(int i=0;i<p.length();i++){
+        if(map.containsKey(p.charAt(i))){
+            int val=map.get(p.charAt(i));
+            val++;
+            map.put(p.charAt(i),val);
+        }
+        else{
+            map.put(p.charAt(i),1);
+        }
+       }
+       
+        HashMap<Character,Integer>map1=new HashMap<>();
+       for(int i=0;i<check.length();i++){
+        if(map1.containsKey(check.charAt(i))){
+            int val1=map1.get(check.charAt(i));
+            val1++;
+            map1.put(check.charAt(i),val1);
+        }
+        else{
+            map1.put(check.charAt(i),1);
+        }
+       }
+       if(map.equals(map1)){
+        return true;
+       }
+
+       int l=0;
+       int r=k-1;
+       while(r<s.length()-1){
+        if(map1.containsKey(s.charAt(l))){
+            int values=map1.get(s.charAt(l));
+            values--;
+            if(values==0){
+                map1.remove(s.charAt(l));
+            }
+            else{
+                map1.put(s.charAt(l),values);
+
+            }
+            
+        }
+        
+        str.delete(0,1);
+        l++;
+        r++;
+        str.append(s.charAt(r));
+        //System.out.println(str);
+        if(map1.containsKey(s.charAt(r))){
+            int values1=map1.get(s.charAt(r));
+            values1++;
+            
+            map1.put(s.charAt(r),values1);
+        }
+        else{
+            map1.put(s.charAt(r),1);
+        }
+        //System.out.println(map1);
+
+        if(map.equals(map1)){
+            return true;
+        }
+
+        
+
+
+       }
+  
+       return false;
+       
+    }
+}
