7c3e20b5a905e17b07d680ea1e2968346ac48991
Arshiyakhias <116972322+Arshiyakhias@users.noreply.github.com>
2025-12-07 21:58:57 +0530

    Time: 14 ms (5.35%), Space: 78.2 MB (60%) - LeetHub

---
+++ 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -0,0 +1,75 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int flip=0;
+        Boolean flips=false;
+        if(nums[0]==0){
+
+        for(int i=0;i<3;i++){
+            if(nums[i]==0){
+                nums[i]=1;
+                flips=true;
+                continue;
+                
+            }
+            
+            if(flips==true){
+                if(nums[i]==0){
+                    nums[i]=1;
+                }
+                else{
+                    nums[i]=0;
+                }
+            }
+
+        }}
+        if(flips==true){
+            flip++;
+        }
+
+     
+       
+
+        int l=0;
+        int r=2;
+        for(int i=0;i<=2;i++){
+            System.out.println(nums[i]);
+        }
+        while(r<nums.length-1){
+            l++;
+            r++;
+            if(nums[l]==0){
+                nums[l]=1;
+                flip++;
+                if(nums[l+1]==0){
+                    nums[l+1]=1;
+
+                }
+                else if(nums[l+1]==1){
+                    nums[l+1]=0;
+                }
+                
+               
+                if(nums[r]==0){
+                    nums[r]=1;
+                }
+                else if(nums[r]==1){
+                    nums[r]=0;
+                }
+                
+            
+            }
+           
+
+        }
+
+        
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                return -1;
+            }
+        }
+        
+        return flip;
+    }
+}
