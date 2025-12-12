f8789cf773de71d2b69b6cca21c74dcfb9dd1ae4
Arshiyakhias <116972322+Arshiyakhias@users.noreply.github.com>
2025-12-12 15:19:35 +0530

    Time: 11 ms (5.75%), Space: 46 MB (17.01%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,18 +1,20 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0){
+        if(x<0)
+        {
             return false;
         }
-        String ans=new String();
-        ans=ans+x;
-        StringBuffer str=new StringBuffer(ans);
-        str.reverse();
-        if(ans.equals(str.toString())){
+        List<Integer>list=new ArrayList();
+        while(x>0){
+            list.add(x%10);
+            x/=10;
+        }
+         List<Integer> rev = new ArrayList<>(list);
+        Collections.reverse(rev);
+        if(list.equals(rev))
+        {
             return true;
         }
         return false;
-
-
-        
     }
 }
