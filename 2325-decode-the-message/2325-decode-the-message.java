d0046eaa08787317db0c756988de023e93f1bf0c
Arshiyakhias <116972322+Arshiyakhias@users.noreply.github.com>
2025-12-14 23:19:04 +0530

    Time: 31 ms (6.02%), Space: 46.7 MB (22.76%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -0,0 +1,59 @@
+class Solution {
+    public String decodeMessage(String key, String message) {
+        System.out.println(key);
+        HashMap<Character,Character>map=new HashMap<>();
+        HashSet<Character>set=new HashSet<>();
+        String key1=new String();
+        for(int i=0;i<key.length();i++){
+            if(set.contains(key.charAt(i))){
+                continue;
+            }
+            else{
+                key1=key1+key.charAt(i);
+                set.add(key.charAt(i));
+
+            }
+        }
+        System.out.println(key1);
+        int index=0;
+        for(char i='a';i<='z';i++){
+            if(key1.charAt(index)==' '){
+                index++;
+                map.put(key1.charAt(index),i);
+                System.out.println(key1.charAt(index));
+                System.out.println(i);
+                System.out.println();
+                index++;
+            }
+            else if(!map.containsKey(key1.charAt(index))){
+                System.out.println("Hello");
+                map.put(key1.charAt(index),i);
+                System.out.println(key1.charAt(index));
+                System.out.println(i);
+                System.out.println();
+                index++;
+            }
+           
+            
+            
+
+        }
+        String ans=new String();
+
+        
+
+
+        for(int i=0;i<message.length();i++){
+            if(map.containsKey(message.charAt(i))){
+                char vals=map.get(message.charAt(i));
+                ans=ans+vals;
+
+            }
+            else{
+                ans=ans+' ';
+            }
+            
+        }
+        return ans;
+    }
+}
