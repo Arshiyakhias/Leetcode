141c393ea6ccc4c47480a8928b2de4e505d68f15
Arshiyakhias <116972322+Arshiyakhias@users.noreply.github.com>
2026-05-29 18:48:02 +0530

    Time: 18 ms (5.02%), Space: 47.4 MB (5.11%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -2,13 +2,37 @@ public class Codec {
 
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-       return longUrl;
+        String encode=new String();
+        if(longUrl.length()==0){
+            encode=encode+'#';
+            encode=encode+'2';
+            return encode;
+
+        }
+        String[]arr=longUrl.split("");
+        
+        for(int i=0;i<arr.length;i++){
+            encode=encode+arr[i];
+            if(i!=arr.length-1){
+                encode=encode+'#';
+                encode=encode+'2';
+            }
+        }
+        return encode;
         
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return shortUrl;
+        String decode=new String();
+        if(decode.length()==2 && decode.equals("#2")){
+            return decode;
+        }
+        String[]arr=shortUrl.split("#2");
+        for(int i=0;i<arr.length;i++){
+            decode=decode+arr[i];
+        }
+        return decode;
     }
 }
 
