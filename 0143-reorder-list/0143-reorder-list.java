568f9a586c7f883d2c7718ed36836c3753d48de4
Arshiyakhias <116972322+Arshiyakhias@users.noreply.github.com>
2026-06-13 22:59:13 +0530

    Time: 5 ms (5.53%), Space: 50.4 MB (7.24%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,30 +10,31 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        ArrayList<Integer>lis=new ArrayList<>();
+        List<Integer>lis=new ArrayList<>();
         ListNode curr=head;
         while(curr!=null){
             lis.add(curr.val);
             curr=curr.next;
         }
-        ListNode curr1=head;
         int left=0;
         int right=lis.size()-1;
-        int count=1;
+        ListNode currs=head;
         while(left<=right){
-            if(count%2!=0){
-                curr1.val=lis.get(left);
-                count++;
-                left++;
-                curr1=curr1.next;
+            if(left==right){
+                currs.val=lis.get(left);
+                break;
+
             }
-            else{
-                curr1.val=lis.get(right);
-                right--;
-                count++;
-                curr1=curr1.next;
-            }
+         
+                            
+            currs.val=lis.get(left);
+            currs.next.val=lis.get(right);
+            
+            currs=currs.next.next;
+            left++;
+            right--;
+
         }
-       
+        
     }
 }
